f5c96237f6b7cf354ef953ce0219ad3461c34ac4
Frederic Spycher <spycher.frederic@gmail.com>
2025-09-10 15:19:11 +0200

    removed optional columns if not needed, improved holdings split

--- README.md
+++ README.md
@@ -1,38 +1,32 @@
 # MARCXML Bibliographic Record Data Mining with Regular Expressions
 
 ## Author
+
 **Frederic Spycher**, librarian at the Fribourg Cantonal and University Library (BCU Fribourg), Switzerland
 
-## Context
-This Python script parses MARCXML files to extract user-specified data in a somewhat efficient manner.
-It is meant to address the lack of straightforward ways to mine specific MARC data from Alma (library management system by Ex Libris).
-This was also a bit of a pet project to further improve my knowledge of regular expressions, which I generally find quite useful when working as a librarian, especially if dealing with data analysis on a regular basis.
+## Description
 
-The code has been written and tested only with MARC 21 XML files as exported by the Alma software.
+This Python script uses regular expressions to extract user-specified data from a MARCXML file in a somewhat efficient manner (faster than with an XML parser like Beautiful Soup).
+The output is a CSV file.
 
-## Regular expressions vs. dedicated XML parser
-The main script, `marcxml_bib_miner.py`, uses regular expressions for data mining. Its lack of dependencies enables its use in any Python 3 environment.
-Furthermore, as there is no need to parse the DOM tree first, the script starts extracting the data right away, no matter the file size.
-Of course, the more features to be extracted, the slower the execution time.
+The script addresses the lack of straightforward ways to mine specific MARC data from Alma (library management system by Ex Libris).
+This was also a bit of a pet project to further improve my knowledge of regular expressions, which I generally find quite useful for library data analysis.
 
-For comparison, a similar script using the lxml parser (via Beautiful Soup 4) was written. As suspected, this script is much slower.
-Using regular expressions, 75,000 records can be parsed under a minute, whereas it takes the lxml parser more than 7 minutes to accomplish the same.
-However, the code may need to be re-written to be more efficient; more experienced programmers should get in touch with me if they have suggestions.
-That being said, there are no plans to continue supporting the lxml version of the script.
+The code has been tested only with MARC 21 XML files as exported by the Alma software.
 
 ## Usage
-The MARC data can be mined at the field or subfield level (or, in case of the leader and control fields, at the position level). It is possible to extract data conditionally based on the value of indicators. Control numbers (field 001) are extracted by default.
+
+The MARC data can be mined at the field or subfield level (or, in the case of the leader and control fields, at the position level).
+It is possible to extract data conditionally based on the value of indicators.
+Control numbers (field 001) are extracted by default.
 
 Optional arguments include:
-- extracting field indicators (which appear in the output between curly brackets);
-- counting the number of physical locations (852), electronic locations (856), and/or local extensions ($9 LOCAL) for each record;
-- splitting the rows by holdings.
 
-The output is a CSV file, which can then serve as input to any data analysis tool.
+- Splitting the rows by holdings
+- Extracting field indicators (which appear in the output between curly brackets)
+- Counting the number of physical locations (852), electronic locations (856), and/or local extensions ($9 LOCAL) for each record
 
 Example of a command:
-`python marcxml_bib_miner.py sample.xml output.csv "LDR/6,264ind1= ind2=4,300,338$b,856ind2=1$u"`
-
-Depending on the terminal, different field-subfield separators should be used such as `_` or `-` (e.g., in PowerShell `$` is reserved for variables and thus won't work).
+`python marcxml_bib_miner.py sample.xml output.csv "LDR/6,264ind1= ind2=4,300,852_h,856ind2=1_u" -s`
 
 See built-in help (-h) for more information on command usage.
--- marcxml_bib_miner.py
+++ marcxml_bib_miner.py
@@ -5,7 +5,6 @@ import re
 import sys
 import time
 
-
 def timer(func):
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
@@ -14,37 +13,28 @@ def timer(func):
         end = time.perf_counter()
         elapsed_time = end - start
         print("Elapsed time: {:0.4f} seconds".format(elapsed_time))
-
         return value
-
     return wrapper_timer
-
 
 def update_progress(n_records: int):
     n_records = str(n_records)
     progress_text = " records parsed"
-
     for i in range(0, len(n_records) + len(progress_text)):
         sys.stdout.write("\b")
-
     sys.stdout.write("{0}{1}".format(n_records, progress_text))
     sys.stdout.flush()
 
-
 def clean_feature(s: str):
-    s = s.lower()
+    s = s.lower().lstrip()
     s = re.sub("ldr", "leader", s)
-    s = re.sub(r"[_\-/$.:]", "_", s)
-
+    s = re.sub(r"[_\-/]", "_", s)
     return s
-
 
 def get_record_regexp():
     pattern = """(?<=<record>)
                  .*?
                  (?=</record>)
                  """
-
     return re.compile(pattern, re.VERBOSE | re.DOTALL)
 
 
@@ -53,7 +43,6 @@ def get_bib_cnum_regexp():
                  (\S+)
                  (?=</controlfield>)
                  """
-
     return re.compile(pattern, re.VERBOSE)
 
 
@@ -69,12 +58,10 @@ def get_fields_with_tag_regexp(f_tag: str):
                      .*?
                      (?=</controlfield|</datafield)
                      """.format(tag=f_tag)
-
     return re.compile(pattern, re.VERBOSE | re.DOTALL)
 
-
 def get_field_contents_regexp(f_tag: str, f_param: str):
-    if f_tag == "leader" or f_tag.startswith("00"):
+    if f_tag == "leader" or f_tag.startswith("00"):  # LDR and control fields
         if f_param == "":
             pattern = """{tag}"?>
                          (.*)  # group 1 (data to be mined)
@@ -98,13 +85,10 @@ def get_field_contents_regexp(f_tag: str, f_param: str):
                          (?=</subfield>)
                          """.format(tag=f_tag, sbf_code=f_param)
         flags = re.VERBOSE | re.DOTALL
-
     return re.compile(pattern, flags)
-
 
 def get_indicators(field):
     return re.findall("ind[1-2]=\"(.)\"", field)
-
 
 def get_all_hol_cnum(record: str):
     pattern = """(?<=tag="852")
@@ -112,9 +96,7 @@ def get_all_hol_cnum(record: str):
                  (\S+)  # group 1 (data to be mined)
                  (?=</subfield)
                  """
-
     return re.findall(re.compile(pattern, re.VERBOSE | re.DOTALL), record)
-
 
 @timer
 def main():
@@ -130,9 +112,14 @@ def main():
                                 the position (for LDR/control fields) 
                                 or the subfield code (for data fields).
                                 Feature separators: [,;]
-                                Parameter separators: [_-/$.:]
-                                Example: "LDR/6,264ind1= ind2=4,300,338$b,856ind2=1$u"
+                                Parameter separators: [_-/]
+                                Example: "LDR/6,264ind1= ind2=4,300,338_b,856ind2=1_u"
                                 """
+                        )
+    parser.add_argument("-s",
+                        "--split_by_hol",
+                        action="store_true",
+                        help="write 1 row per physical holdings (requires embedded holdings data)"
                         )
     parser.add_argument("-i",
                         "--show_ind",
@@ -154,11 +141,6 @@ def main():
                         action="store_true",
                         help="count local extensions ($9 LOCAL)"
                         )
-    parser.add_argument("-s",
-                        "--split_by_holdings",
-                        action="store_true",
-                        help="write 1 row per holdings (requires embedded holdings data)"
-                        )
     args = parser.parse_args()
     filename_in = args.input_filename
     filename_out = args.output_filename
@@ -167,30 +149,27 @@ def main():
     # File handling
     f_in = open(filename_in, "r", encoding="utf-8")
     f_out = open(filename_out, "w", encoding="utf-8", newline="")
-    header = ["bib_cnum",
-              "hol_cnum",
-              "n_ploc",
-              "n_eloc",
-              "n_local_ext"
-              ] + features
+    header = ["bib_cnum"] + features
+    for arg, fieldname in [(args.count_ploc, "n_local_ext"),
+                           (args.count_eloc, "n_eloc"),
+                           (args.count_local_ext, "n_ploc"),
+                           (args.split_by_hol, "hol_cnum")
+                           ]:
+        if arg:
+            header.insert(1, fieldname)
     writer = csv.DictWriter(f_out, fieldnames=header)
     writer.writeheader()
 
+    # Iterate records
     rec_n = 0
-
     for record_match in re.finditer(get_record_regexp(), f_in.read()):
-        rec_n += 1
-
-        if rec_n == 1 or rec_n % 100 == 0:
-            update_progress(rec_n)
-
         record = record_match.group(0)
-        dict_out = {}
+        row = {}
 
         # Get bib control number
         m = re.search(get_bib_cnum_regexp(), record)
         bib_cnum = m.group(0) if m is not None else ""
-        dict_out["bib_cnum"] = bib_cnum
+        row["bib_cnum"] = bib_cnum
 
         # Extract features
         for feature in features:
@@ -199,59 +178,64 @@ def main():
             tag = tag_ind[0]
             ind_filter = tag_ind[1:]
             param = feat_parts[1] if len(feat_parts) > 1 else ""
-            data = []
-
             select_fields = re.findall(get_fields_with_tag_regexp(tag), record)
-
+            data = []
             for field in select_fields:
                 inds = get_indicators(field)
-
-                if args.show_ind:
-                    if len(inds) > 0:
-                        data.append("{" + "".join(inds) + "}")
-
                 ind_check_passed = True
-                
                 if len(ind_filter) > 0:
                     for ind in ind_filter:
                         ind_n, ind_val = re.split("=", ind)
 
                         if inds[int(ind_n)-1] != ind_val:
                             ind_check_passed = False
-
                 if ind_check_passed:
                     m = re.findall(get_field_contents_regexp(tag, param), field)
-                    data.append(" ".join(m))
 
-            dict_out[feature] = "|".join(data)
+                    if args.show_ind and len(inds) > 0:
+                        data.append("{" + "".join(inds) + "}" + " ".join(m))
+                    else:
+                        data.append(" ".join(m))
+            row[feature] = "|".join(data)
 
         # Count physical locations (= holdings)
         if args.count_ploc:
-            dict_out["n_ploc"] = len(re.findall("tag=\"852\"", record))
+            row["n_ploc"] = len(re.findall("tag=\"852\"", record))
 
         # Count electronic locations (= URL)
         if args.count_eloc:
-            dict_out["n_eloc"] = len(re.findall("tag=\"856\"", record))
+            row["n_eloc"] = len(re.findall("tag=\"856\"", record))
 
         # Count local extensions
         if args.count_local_ext:
             regexp = re.compile("(?<=code=\"9\">)local", re.IGNORECASE)
-            dict_out["n_local_ext"] = len(re.findall(regexp, record))
+            row["n_local_ext"] = len(re.findall(regexp, record))
 
         # Write to file output
-        if args.split_by_holdings:
-            for hol_cnum in get_all_hol_cnum(record):
-                dict_out["hol_cnum"] = hol_cnum
-                writer.writerow(dict_out)
+        if args.split_by_hol:
+            for i, hol_cnum in enumerate(get_all_hol_cnum(record)):
+                row["hol_cnum"] = hol_cnum
+                row_cp = row.copy()
+                for k, v in row.items():
+                    if k.startswith(("84", "85", "86", "87")) and not k.startswith(("856", "857")):
+                        parts = v.split("|")
+                        if i < len(parts):
+                            row_cp[k] = parts[i]
+                        else:
+                            row_cp[k] = ""
+                writer.writerow(row_cp)
         else:
-            writer.writerow(dict_out)
+            writer.writerow(row)
+
+        # Update progress
+        rec_n += 1
+        if rec_n == 1 or rec_n % 100 == 0:
+            update_progress(rec_n)
 
     update_progress(rec_n)
     sys.stdout.write("\n")
-
     f_in.close()
     f_out.close()
-
 
 if __name__ == "__main__":
     main()
